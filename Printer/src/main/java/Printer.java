abda8932c34c1cc3a15989270d16609ce89819b6
Keith Allan <keithallan01@gmail.com>
2019-02-11 16:57:36 +0000

    extensions complete

--- Printer/src/main/java/Printer.java
+++ Printer/src/main/java/Printer.java
@@ -1,15 +1,29 @@
 public class Printer {
     private int sheets;
+    private int volume;
 
-    public Printer(int sheets){
+    public Printer(int sheets, int volume){
         this.sheets = sheets;
+        this.volume= volume;
     }
 
     public int sheetsRemaining(){
        return this.sheets;
     }
 
-    public void print(int numPages, int numCopies){
-        this.sheets -= numPages * numCopies;
+
+    public void print(int numPages, int numCopies) {
+        if (this.sheets >= numPages * numCopies) {
+            this.sheets -= numPages * numCopies;
+            this.volume -= numPages * 1;
+        }
+    }
+
+    public void fill(){
+        this.sheets = 20;
+    }
+
+    public int volume(){
+        return this.volume;
     }
 }
--- Printer/src/test/java/PrinterTest.java
+++ Printer/src/test/java/PrinterTest.java
@@ -8,14 +8,43 @@ public class PrinterTest {
 
     @Test
     public void hasSheetsRemaining(){
-        printer = new Printer(20);
+        printer = new Printer(20, 50);
         assertEquals(20, printer.sheetsRemaining());
     }
 
     @Test
     public void canPrint(){
-        printer = new Printer(20);
-        printer.print(2,5);
+        printer = new Printer(20, 50);
+        printer.print(3,5);
+        assertEquals(5, printer.sheetsRemaining());
+    }
+
+    @Test
+    public void insufficientPaper(){
+        printer = new Printer(10, 50);
+        printer.print(7,2);
         assertEquals(10, printer.sheetsRemaining());
     }
+
+
+    @Test
+    public void canFill(){
+        printer = new Printer(10, 50);
+        printer.fill();
+        assertEquals(20, printer.sheetsRemaining());
+    }
+
+    @Test
+    public void hasTonerVolume(){
+        printer = new Printer(10, 50);
+        assertEquals(50, printer.volume());
+    }
+
+    @Test
+    public void reduceToner(){
+        printer = new Printer(10, 50);
+        printer.print(5, 1);
+        assertEquals(45, printer.volume());
+    }
+
 }
